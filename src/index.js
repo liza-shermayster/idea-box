1712f64cc2ca25af0141f78101d8e1b31e29a846
Liza Shermayster <lizashermayster@Lizas-MacBook-Pro.local>
2020-05-17 21:50:22 +0300

    added tailwind

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./tailwind.css";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
-import awsconfig from './aws-exports';
+import awsconfig from "./aws-exports";
 Amplify.configure(awsExports);
 
 Amplify.configure(awsconfig);
