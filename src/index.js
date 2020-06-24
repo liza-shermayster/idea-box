6118f379b4d5b186a5c0a3b3d3442be9f0832894
Pavel Shermayster <sherm84@gmail.com>
2020-06-24 10:52:24 +0300

    build fix

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import Amplify from "aws-amplify";
-import awsExports from "./aws-exports";
-import awsconfig from './aws-exports';
-Amplify.configure(awsExports);
-
-Amplify.configure(awsconfig);
 
 ReactDOM.render(
   <React.StrictMode>
