6b542ec97666ee45b96fe4a6909c56a925a7c371
Pavel Shermayster <sherm84@gmail.com>
2020-06-24 11:26:16 +0300

    feat: added deletion

--- src/Category.js
+++ src/Category.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Machine, assign } from "xstate";
 import { useMachine } from "@xstate/react";
+import { v4 as uuidv4 } from "uuid";
 
 const categoryMachine = Machine({
   id: "fetch",
   initial: "edit",
   context: {
-    items: [],
+    items: {},
   },
   states: {
     edit: {
@@ -14,7 +15,24 @@ const categoryMachine = Machine({
         ADD_ITEM: {
           target: "edit",
           actions: assign({
-            items: (ctx, e) => [...ctx.items, e.item],
+            items: (ctx, e) => {
+              const itemKey = uuidv4();
+              const newItem = { _key: itemKey, value: e.item };
+              const items = { ...ctx.items };
+              items[itemKey] = newItem;
+              return { ...items };
+            },
+          }),
+        },
+        DELETE_ITEM: {
+          target: "edit",
+          actions: assign({
+            items: (ctx, e) => {
+              const { itemKey } = e;
+              const items = { ...ctx.items };
+              delete items[itemKey];
+              return items;
+            },
           }),
         },
       },
@@ -24,31 +42,50 @@ const categoryMachine = Machine({
 
 export function Category() {
   const [current, send] = useMachine(categoryMachine);
-  const [itemValue, setItemValue] = React.useState("");
   const { items } = current.context;
-
+  console.log(items);
   return (
     <div>
       <ul>
-        {items.map((i) => (
-          <li key={i}>{i}</li>
+        {Object.keys(items).map((itemKey) => (
+          <li key={itemKey}>
+            {items[itemKey].value}{" "}
+            <button
+              onClick={() => {
+                send({ type: "DELETE_ITEM", itemKey });
+              }}
+            >
+              delete
+            </button>
+          </li>
         ))}
       </ul>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (itemValue === "") return;
+      <AddItemForm
+        onSubmit={(itemValue) => {
           send({ type: "ADD_ITEM", item: itemValue });
-          setItemValue("");
         }}
-      >
-        <input
-          value={itemValue}
-          onChange={(e) => setItemValue(e.target.value)}
-          id="itemName"
-        />
-        <button type="submit">Create new item</button>
-      </form>
+      />
     </div>
+  );
+}
+
+function AddItemForm({ onSubmit }) {
+  const [itemValue, setItemValue] = React.useState("");
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (itemValue === "") return;
+        onSubmit(itemValue);
+        setItemValue("");
+      }}
+    >
+      <input
+        value={itemValue}
+        onChange={(e) => setItemValue(e.target.value)}
+        id="itemName"
+      />
+      <button type="submit">Create new item</button>
+    </form>
   );
 }
